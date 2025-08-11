14afbaf88f58b1719bb48c1eabf15818d203a615
Alberto Schneider <albertoschneider891@gmail.com>
2025-08-11 11:01:17 -0300

    Criacao de novas telas e os layouts agora sao clicaveis

--- app/src/main/java/com/instituto/bancodealimentos/menu.java
+++ app/src/main/java/com/instituto/bancodealimentos/menu.java
@@ -1,6 +1,9 @@
 package com.instituto.bancodealimentos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -19,6 +22,26 @@ public class menu extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // Agora sim, já que o layout foi carregado
+        LinearLayout card1 = findViewById(R.id.card1);
+        LinearLayout card2 = findViewById(R.id.card2);
+        LinearLayout card3 = findViewById(R.id.card3);
+
+        card1.setOnClickListener(v -> {
+            Intent intent = new Intent(menu.this, pontosdecoleta.class);
+            startActivity(intent);
+        });
+
+        card2.setOnClickListener(v -> {
+            Intent intent = new Intent(menu.this, doealimentos.class);
+            startActivity(intent);
+        });
+
+        card3.setOnClickListener(v -> {
+            Intent intent = new Intent(menu.this, voluntariar.class);
+            startActivity(intent);
         });
     }
 }
