047ab70d0e36ea34ee95413b51a89993e14b4c87
alberto.schneider <albertoschneider891@gmail.com>
2025-08-21 14:42:51 -0300

    Alguns layouts e erros corrigidos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
 
 dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.0.0"))
+    implementation ("com.google.android.material:material:1.12.0")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
